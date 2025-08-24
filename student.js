e9e718f3edd3ccddf685b403140835f8bf11a830
Sakshi mishra <sakshimishra41777@gmail.com>
2025-08-25 00:02:10 +0530

    Add files via upload

---
+++ student.js
@@ -0,0 +1,37 @@
+// student.js
+
+// Create a student object
+const students = [
+    {
+        name: "Nitin Rajput",
+        age: 20,
+        branch: "Artificial Intelligence & Data Science",
+        rollNo: "AIDS123",
+        college: "RGPV"
+    },
+    {
+        name: "Pawan Parmar",
+        age: 22,
+        branch: "Artificial Intelligence & Data Science",
+        rollNo: "AIDS123",
+        college: "RGPV"
+    },
+    {
+        name: "Sakshi Mishra",
+        age: 19,
+        branch: "Artificial Intelligence & Data Science",
+        rollNo: "AIDS123",
+        college: "RGPV"
+    }
+];
+// Print student details
+students.forEach((student, idx) => {
+    console.log(`----- Student ${idx + 1} Details -----`);
+    console.log("Name: " + student.name);
+    console.log("Age: " + student.age);
+    console.log("Branch: " + student.branch);
+    console.log("Roll No: " + student.rollNo);
+    console.log("College: " + student.college);
+});
+console.log("Roll No: " + students[0].rollNo);
+console.log("College: " + students[0].college);
